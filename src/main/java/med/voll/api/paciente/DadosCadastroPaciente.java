da092615293b01b78f5e6498dda7a9a7f9cea698
jrmesquita7 <cleobetojunior1999@gmail.com>
2023-10-27 20:23:43 -0300

    first commit

---
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,7 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+}
